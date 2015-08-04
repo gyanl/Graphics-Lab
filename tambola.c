fe1526314930a307678dea97e127a3810034942a
Gyan Lakhwani <gyanlakhwani@gmail.com>
2015-08-04 14:42:57 +0530

    Create tambola.c

---
+++ tambola.c
@@ -0,0 +1,77 @@
+/*
+Version 1
+Gyan Lakhwani
+4 August 2015
+*/
+
+#include<conio.h>
+#include<stdlib.h>
+
+void reprint();
+int tic1[10];
+int tic2[10];
+int i=0,j=0;
+void main()
+{
+
+int x=0,n=0,p1=0,p2=0,a=0,p=0,done[100];
+for(p=0;p<100;p++)
+{done[p]=0;}
+clrscr();
+for(j=0;j<100;j=j+10)
+  {
+    tic1[i]=((rand())%10)+j;
+    tic2[i]=((rand())%10)+j;
+    i++;
+  }
+reprint();
+
+for(n=0;n<100;n++)
+{
+  x=rand()%100;
+  done[x]=1;
+  printf("\n\nNUMBER : %d",x);
+  for(a=0;a<10;a++)
+    {
+      if(x==tic1[a])
+	{
+	  p1++;
+	  printf("\nP1 has %d.",x);
+	  delay(500);
+	}
+      if(x==tic2[a])
+	{
+	  p2++;
+	  printf("\nP2 has %d.",x);
+	  delay(500);
+	}
+    }
+    printf("\n\n");
+    for(p=0;p<100;p++) {if (done[p]) printf("%d ",p);
+    if(p1==10) {printf("\nP1 WINS!!!");delay(2000);exit(0);}
+    if(p2==10) {printf("\nP2 WINS!!!");delay(2000);exit(0);}
+    }
+    delay(200);reprint();
+}
+getch();
+}
+
+
+/* reprint */
+
+
+void reprint()
+{
+clrscr();
+printf("\nPlayer 1 : ");
+for(i=0;i<10;i++)
+  {
+    printf("%d ",tic1[i]);
+  }
+
+printf("\nPlayer 2 : ");
+for(i=0;i<10;i++)
+  {
+    printf("%d ",tic2[i]);
+  }
+}
